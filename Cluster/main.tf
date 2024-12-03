8c30f9aa4572a02348f1cce9c4b680f7b689afa5
AmbatiSaiKrishnaR <130149208+AmbatiSaiKrishnaR@users.noreply.github.com>
2024-12-03 23:39:55 +0530

    Update main.tf

--- Cluster/main.tf
+++ Cluster/main.tf
@@ -111,7 +111,7 @@ resource "aws_eks_node_group" "devopsshack" {
   instance_types = ["t2.large"]
 
   remote_access {
-    ec2_ssh_key = var.ssh_key_name
+    ec2_ssh_key = "project" # Updated to use your existing key pair name
     source_security_group_ids = [aws_security_group.devopsshack_node_sg.id]
   }
 }
@@ -152,24 +152,4 @@ resource "aws_iam_role" "devopsshack_node_group_role" {
       "Principal": {
         "Service": "ec2.amazonaws.com"
       },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_role_policy_attachment" "devopsshack_node_group_role_policy" {
-  role       = aws_iam_role.devopsshack_node_group_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-}
-
-resource "aws_iam_role_policy_attachment" "devopsshack_node_group_cni_policy" {
-  role       = aws_iam_role.devopsshack_node_group_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-}
-
-resource "aws_iam_role_policy_attachment" "devopsshack_node_group_registry_policy" {
-  role       = aws_iam_role.devopsshack_node_group_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-}
+      "Action": "sts:AssumeRole
